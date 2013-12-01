728898555c3c6f488b257b70d86596bdfa53c48d
Chk2 <chikin202@gmail.com>
2013-12-01 20:28:10 +0100

    made 3 lines algorithm with for loops, uncle Bob's style

--- tests/Chk2/PrimeFactors/PrimeFactorsTest.php
+++ tests/Chk2/PrimeFactors/PrimeFactorsTest.php
@@ -36,19 +36,18 @@ class PrimeFactorsTest extends \PHPUnit_Framework_TestCase {
 }
 
 /**
+ * Uncle Bob's style
+ *
  * @param int $n
  * @return int[]
  */
 function of($n)
 {
     $list = [];
-    $divisor = 2;
-    while ($n > 1) {
-        while ($n % $divisor == 0) {
+
+    for ($divisor = 2; $n > 1; $divisor++)
+        for (; $n % $divisor == 0; $n /= $divisor)
             $list[] = $divisor;
-            $n /= $divisor;
-        }
-        $divisor++;
-    }
+
     return $list;
 }
